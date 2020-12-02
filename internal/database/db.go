cf56d03f8f35176f67f4967f002509b81ee31990
Pavel Smejkal <pavel.smejkal@outreach.io>
2020-12-02 13:34:09 +0100

    moving span for db queries

--- internal/database/db.go
+++ internal/database/db.go
@@ -25,13 +25,19 @@ func NewDB(stats *stats.Stats) *DB {
 
 // Load loads a number from a fake database.
 // Normally, we would propagate the trace ID to the database.
-func (db *DB) Query(ctx context.Context, name string, n int) (int, error) {
+func (db *DB) Query(ctx context.Context, name string, n int) (r int, err error) {
+	ctx, span := beeline.StartSpan(ctx, "query")
+	defer func() {
+		if err != nil {
+			span.AddField("error", err.Error())
+		}
+		span.Send()
+	}()
+
 	if util.Lucky(0.1) {
 		return 0, errors.New("query." + name)
 	}
 
-	ctx, span := beeline.StartSpan(ctx, "query")
-	defer span.Send()
 	span.AddField("query_name", name)
 
 	db.stats.Count("database.query", 1, []string{"name:load"})
